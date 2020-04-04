274e8b9671af0a6f11f8ba876f6fb403ede18df9
railek <jean@railek.com>
2020-04-05 01:22:43 +0200

    docs: commit message guidelines & changing date

--- src/mdx/code.jsx
+++ src/mdx/code.jsx
@@ -9,6 +9,8 @@ const StyledPre = styled.pre`
 	border-radius: var(--size-6);
 	border: 1px solid var(--gray-200);
 	margin: var(--size-24) var(--size-0);
+	white-space: normal !important;
+	word-break: break-word !important;
 `;
 
 export default function Code(props) {
